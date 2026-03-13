dc513ed94148fb69ffa639733f22fcd5fdda7ed5
yaretzipang <ypang@connect.kellettschool.com>
2026-03-13 06:28:14 +0000

    Mar 13, 2026, 2:28 PM

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 ## Personal Programming Project - Yaretzi Pang
+import os, time
 
 
+def clear_screen():
+    for i in range(3):
+        print(".")
+        time.sleep(3)
+    os.system("clear")
 
-# hello
+
+print("""--------------
+     UNO
+--------------""")
+num_of_players = int(input("How many players are playing (pick a number from 2-4)\n"))
