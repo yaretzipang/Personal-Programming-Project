eee558ccbed320d7455e9980d60e951e72bc929a
yaretzipang <ypang@connect.kellettschool.com>
2026-03-13 06:36:47 +0000

    Mar 13, 2026, 2:36 PM

--- main.py
+++ main.py
@@ -11,8 +11,21 @@ def clear_screen():
     os.system("cls")
 
 
+def create_players(num_of_players):
+
+    for i in range(num_of_players):
+        player_[i] = Player
+
+
+class Player:
+
+    player_cards = []
+    num_of_cards = 0
+
+
 print("""--------------
      UNO
 --------------""")
 num_of_players = int(input("How many players are playing (pick a number from 2-4)\n"))
 clear_screen()
+create_players(num_of_players)
