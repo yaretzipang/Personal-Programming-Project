2bfcd1d5fce4666cda78f2b0b6c09c34d646fb85
yaretzipang <ypang@connect.kellettschool.com>
2026-03-13 07:09:04 +0000

    Mar 13, 2026, 3:09 PM

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 ## Personal Programming Project - Yaretzi Pang
 import os, time
 
-
 players = []
 player_turn = 1
+colour_list = ["R", "Y", "G", "B", "S"]
+numbers = 
 
 class Player:
 
@@ -29,13 +30,36 @@ def create_players():
         players.append(player)
 
 
-def check_player_turn():
-    pass
+def player_turn():
+    
+    output_player_cards()
+
+
+def output_player_cards():
+    get_card_list()
+
+
+def get_card_list():
+
+    if generating = True:
+        generate_cards()
+
+    if player_turn = 0:
+        players[0].player_cards = 
+
+
+def generate_cards():
+
+    if player_turn = 0:
+        players[0].player_cards = []
+
+
 
 print("""--------------
      UNO
 --------------""")
 num_of_players = int(input("How many players are playing (pick a number from 2-4)\n"))
 clear_screen()
+generating = True
 create_players()
-check_player_turn()
+player_turn()
